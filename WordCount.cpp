8298f70266a74172f5e43d016456dcb4c9a317be
zlx-cmd <65639972+zlx-cmd@users.noreply.github.com>
2020-05-22 14:40:38 +0800

    Update WordCount.cpp

--- WordCount.cpp
+++ WordCount.cpp
@@ -1,34 +1,147 @@
-﻿// WordCount.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+// WordCount.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<malloc.h>
+#include<io.h>
 
-int main()
-{
-	FILE* fp;
-	int cnum = 0;
-	int wnum = 1;
-	char ch;
-	if ((fp = fopen("input_file_name.txt", "r")) == NULL)
+void Scan(char* Path, char Type);
+
+int charcount(char* Path) 
+{	
+	FILE* file = fopen(Path, "r");
+	if (file != NULL)
 	{
-		printf("打开文件失败\n");
-		return;
+		printf("文件打开失败！");
+		exit(0);
 	}
-	while ((ch = fgetc(fp)) != EOF)
+	char c;
+	int count = 0;
+	while ((c = fgetc(file)) != EOF)
+		count = ((c != ' ') && (c != '\n') && (c != '\t')) + count;
+	fclose(file);
+	return count;
+}
+
+int wordcount(char* Path) 
+{	
+	FILE* file = fopen(Path, "r");
+	if (file != NULL) 
 	{
-		cnum++;
-		if (ch == ',' || ch == ' ')
-		{
-			wnum++;
+		printf("文件打开失败！");
+		exit(0);
+	}
+	char w;
+	int isword = 0;	
+	int count = 0;
+	while ((w = fgetc(file)) != EOF) 
+	{
+		if ((w >= 'a' && w <= 'z') || (w >= 'A' && w <= 'Z')) 
+		{				
+			count = (isword == 0) + count;
+			isword = 1;	
 		}
+		else
+			isword = 0;	
 	}
-	printf("wordCount.exe -c input.txt\n");
-	printf("字符数：%d\n", cnum);
-	printf("wordCount.exe -w input.txt\n");
-	printf("单词数：%d\n", wnum);
-	fclose(fp);
+	fclose(file);
+	return count;
+}
+
+void Run(char Type, char Type2, char* Path) 
+{
+	switch (Type) 
+	{
+	case 'c': 
+		printf("\ncode count: %d\n", charcount(Path));
+		printf("word count: %d\n", wordcount(Path)); 
+		break;
+	case 'w': 
+		printf("word count: %d\n", wordcount(Path)); 
+		break;
+	case 's': 
+		Scan(Path, Type2); 
+		break;
+	default: 
+		printf("type input error"); 
+		break;
+	}
+}
+
+void Scan(char* Path, char Type) 
+{
+	char* FileName = NULL;
+	char* FileType = NULL;
+	char Temp[30];	
+	long Head;
+	struct _finddata_t FileData;
+	int i = 0;
+	FileName = Path;
+	while (*(Path + i) != '\0')
+	{	
+		if (*(Path + i) == '\\')
+			FileName = Path + i + 1;
+		if (*(Path + i) == '.')
+			FileType = Path + i + 1;
+		i++;
+	}
+	strcpy(Temp, FileType);
+	*FileType = '*';
+	*(FileType + 1) = '\0';
+	Head = _findfirst(Path, &FileData);
+	strcpy(FileType, Temp);
+	do {
+		if (!strcmp(FileData.name, "..") || !strcmp(FileData.name, "."))	
+			continue;
+		if (_A_SUBDIR == FileData.attrib)	
+		{
+			strcpy(Temp, FileName);	
+			for (i = 0; *(FileData.name + i) != '\0'; i++)
+			{
+				*(FileName + i) = *(FileData.name + i);
+			}
+			*(FileName + i) = '\\';
+			*(FileName + i + 1) = '\0';
+			strcat(Path, Temp);
+
+			Scan(Path, Type);
+
+			strcpy(FileName, Temp);				
+		}
+		else 
+		{
+			for (i = 0; *(FileData.name + i) != '.'; i++);
+			if (!strcmp(FileData.name + i + 1, FileType)) 
+			{	
+
+				strcpy(Temp, FileName);
+				strcpy(FileName, FileData.name); 
+
+				printf("%s:  ", FileData.name);
+				Run(Type, NULL, Path);
+				printf("\n");
+
+				strcpy(FileName, Temp);
+			}
+		}
+	} while (_findnext(Head, &FileData) == 0);
+	_findclose(Head);
+}
+
+int main(int argc,char* argv[])
+{
+	char Path[100] = "*.txt";
+	char Type = 's';
+	char Type2 = 'c';
+	Run(Type, Type2, Path);
+	getchar();
 	return 0;
 }
+
+
+
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
